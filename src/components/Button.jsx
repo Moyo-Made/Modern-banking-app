3ce57a52b94da3e3b20d97775dcd6052cf2e9b1b
Moyomade <adegbitemoyomade2004@gmail.com>
2024-03-08 17:04:08 +0100

    Code: added button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ const Button = ({styles}) => {
 		<button
 			type="button"
 			className={`py-3 px-6 bg-blue-gradient font-poppins font-medium 
-    text-[18px] text-primary outline-none ${styles}`}
+    text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
 		>
       Get Started
     </button>
